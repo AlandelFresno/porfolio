34a54a1aad26318dc0ede58a5ff21f87cb0e297e
Alan del Fresno <alandelfresno2@gmail.com>
2022-04-10 23:05:16 -0300

    MainPage sketch
    Clear Readme

--- src/MainPage.js
+++ src/MainPage.js
@@ -5,8 +5,19 @@ const MainPage = () => {
   return (
     <div className='h-screen w-screen bg-gray-900 text-white'>
         <Welcome />
+        <div className='flex flex-col '>
+          <div className='flex justify-center'>
+            <h3> Presentation </h3>    
+          </div> 
+          <div className='flex justify-center'>
+            <h3> Projects </h3>    
+          </div> 
+          <div className='flex justify-center'>
+            <h3> Skills </h3>    
+          </div> 
+        </div>
         <div className='flex justify-center'>
-          <h4> Content </h4>
+          <h4> About </h4>
         </div>
         <footer className='flex justify-end pr-4'> copyright 2022 </footer>
     </div>
